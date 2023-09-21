be1ec5341e62ff5dd927cb834931b2fc3e6c3d9b
Vivek Suyal <viveksuyal37@gmail.com>
2023-09-21 16:29:19 +0530

    added news

--- components/Dashboard/ExpandBtn.js
+++ components/Dashboard/ExpandBtn.js
@@ -2,29 +2,43 @@
 
 /*BuiltIn imports */
 import Image from "next/image";
+import { useState } from "react";
 
 /*redux */
 import { useDispatch } from "react-redux";
 import { toggleCard } from "@/Redux/slices/CardSlice";
+import classNames from "classnames";
 
 const ExpandBtn = ({ cardId }) => {
+  const [isCurrentItemExpanded, setIsCardItemExpanded] = useState(false);
   const dispatch = useDispatch();
 
   return (
     <div
       onClick={() => {
         cardId
-          ? dispatch(toggleCard({ cardId }))
+          ? (dispatch(toggleCard({ cardId })), setIsCardItemExpanded(!isCurrentItemExpanded))
           : console.log("no cardId found!!");
       }}
-      className="w-[22px] h-[22px] bg-[#454545] cursor-pointer flex items-center justify-center rounded-full "
+      className={classNames(
+        "w-[22px] h-[22px]  cursor-pointer flex items-center justify-center rounded-full ",
+        { "bg-[#454545]": !isCurrentItemExpanded }
+      )}
     >
-      <Image
-        src="/assets/images/dashboard/expand.png"
-        alt="expand button"
-        width="12"
-        height="12"
-      />
+      {isCurrentItemExpanded && (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="arrows">
+          <path d="M11.29,9.71a1,1,0,0,0,1.42,0l5-5a1,1,0,1,0-1.42-1.42L12,7.59,7.71,3.29A1,1,0,0,0,6.29,4.71Zm1.42,4.58a1,1,0,0,0-1.42,0l-5,5a1,1,0,0,0,1.42,1.42L12,16.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path>
+        </svg>
+      )}
+
+      {!isCurrentItemExpanded && (
+        <Image
+          src="/assets/images/dashboard/expand.png"
+          alt="expand button"
+          width="12"
+          height="12"
+        />
+      )}
     </div>
   );
 };
--- components/Dashboard/SocialMediaAccounts/AccountsContainer.js
+++ components/Dashboard/SocialMediaAccounts/AccountsContainer.js
@@ -7,16 +7,17 @@ const AccountsContainer = ({ expandedCard }) => {
   return (
     <div
       className={classNames(
-        "flex gap-[21px]  transition-all opacity-1 duration-300 ease-in-out overflow-hidden",
+        "flex gap-[21px] max-h-[98.5px]  transition-all opacity-1 duration-300 ease-in-out overflow-hidden",
         {
           " w-0 h-0 opacity-0 hidden": expandedCard,
           "w-full h-full": !expandedCard,
         }
       )}
     >
-      {!expandedCard && ["", "", "", "", ""].map((i, indx) => {
-        return <SocialMediaCard key={indx} />;
-      })}
+      {!expandedCard &&
+        ["", "", "", "", ""].map((i, indx) => {
+          return <SocialMediaCard key={indx} />;
+        })}
 
       <AddSocials />
     </div>
