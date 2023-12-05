30d1235e3d212872eb86525bfab229a7e5be96ab
Vivek Suyal <viveksuyal37@gmail.com>
2023-12-05 10:43:56 +0530

    added pass in rootLayout

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,28 @@ import { store } from '@/Redux/store';
 import './globals.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 
+/*constants*/
+import { APP_PASSWORD } from '@/constants/password';
+
 export default function RootLayout({ children }) {
+  // add password auth on production env
+  if (process.env.NODE_ENV === 'production') {
+    const password =
+      typeof window === 'object'
+        ? prompt('Enter the password to proceed.')
+        : null;
+
+    if (APP_PASSWORD !== password) {
+      return (
+        <html lang='en'>
+          <body className='flex items-center justify-center w-screen h-screen'>
+            <h1>Haha nice try.</h1>
+          </body>
+        </html>
+      );
+    }
+  }
+
   return (
     <html lang='en'>
       <Provider store={store}>
