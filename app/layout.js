8f15276bef6b4083e94ebe038b5a9e2f8783e4a9
Vivek Suyal <viveksuyal37@gmail.com>
2023-12-05 12:33:12 +0530

    app password fixes

--- app/layout.js
+++ app/layout.js
@@ -13,30 +13,31 @@ import { store } from '@/Redux/store';
 /*styles*/
 import './globals.css';
 import 'react-toastify/dist/ReactToastify.min.css';
-
-/*constants*/
-import { APP_PASSWORD } from '@/constants/password';
+import { useEffect, useState } from 'react';
 
 export default function RootLayout({ children }) {
-  // add password auth on production env
-  if (process.env.NODE_ENV === 'production') {
-    const password =
-      typeof window === 'object'
-        ? prompt('Enter the password to proceed.')
-        : null;
+  const [content, setContent] = useState(null);
 
-    if (APP_PASSWORD !== password) {
-      return (
-        <html lang='en'>
-          <body className='flex items-center justify-center w-screen h-screen'>
-            <h1>Haha nice try.</h1>
-          </body>
-        </html>
-      );
+  useEffect(() => {
+    // add password auth on production env
+    if (process.env.NODE_ENV === 'production') {
+      const password = prompt('Enter the password to proceed.');
+
+      if (process.env.NEXT_PUBLIC_APP_PASSWORD !== password) {
+        setContent(
+          <html lang='en'>
+            <body className='flex items-center justify-center w-screen h-screen'>
+              <h1>Haha nice try.</h1>
+            </body>
+          </html>,
+        );
+      }
     }
-  }
+  }, []);
 
-  return (
+  return content ? (
+    content
+  ) : (
     <html lang='en'>
       <Provider store={store}>
         <body className={`${fonts.urbanist.className}`}>
